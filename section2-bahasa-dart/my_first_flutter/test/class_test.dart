4e08a6d4907efb42a90728ea252f8111ad0f7647
yogaprasetia <yogaprasettia@gmail.com>
2022-09-07 10:53:22 +0700

    second commit

---
+++ section2-bahasa-dart/my_first_flutter/test/class_test.dart
@@ -0,0 +1,68 @@
+void main() {
+
+  // // positional argument
+  // var pakaian1 = Pakaian("Baju","Biru");
+  // print("Jenis Pakaianku adalah ${pakaian1.jenis} dan warnanya ${pakaian1.warna}");
+
+  // // named argument
+  // var pakaian2 = Pakaian(jenisNya: "Kemeja", warnaNya: "Hitam");
+  // print("Jenis Pakaianku adalah ${pakaian2.jenis} dan warnanya ${pakaian2.warna}");
+  
+  // // direct named argument constructor
+  var pakaian2 = Pakaian(jenis: "Kemeja", warna: "Hitam", ukuran: "M");
+  print("Jenis Pakaianku adalah ${pakaian2.jenis} dan warnanya ${pakaian2.warna}");
+}
+
+class Pakaian {
+  // atribut
+  // private -> ( _ )
+  String? jenis;
+  String? warna;
+  String? _ukuran;
+
+  // constructor
+  // positional argument
+  // Pakaian(String jenisNya, String warnaNya) {
+  //   jenis = jenisNya;
+  //   warna = warnaNya;
+  // }
+
+  // constructor
+  // named argument
+  // Pakaian({String? jenisNya, String? warnaNya}) {
+  //   jenis = jenisNya;
+  //   warna = warnaNya;
+  // }
+
+  // direct named argument constructor
+  Pakaian({this.jenis, this.warna, ukuran}){
+    _ukuran = ukuran;
+  }
+
+  // Method
+  // fungsi set biasa
+  void gantiUkuran(String? newUkuran){
+    _ukuran = newUkuran;
+  }
+
+  // fungsi get biasa
+  // String? ukuran() {
+  //   return _ukuran;
+  // }
+
+  // getter
+  String? get ukuran {
+    return _ukuran;
+  }
+
+  // setter
+  set ukuran(String? newUkuran) {
+    _ukuran = newUkuran;
+  }
+
+  // // direct positional argument constructor
+  // Pakaian(this.jenis, this.warna, this.ukuran);
+
+
+
+}
--- section2-bahasa-dart/my_first_flutter/test/null_safety_test.dart
+++ section2-bahasa-dart/my_first_flutter/test/null_safety_test.dart
@@ -1,3 +1,28 @@
+// Null Safety -> fitur untuk mengetahui adanya error akibat null
+// ada 3 jenis syntax baru :
+// ? ! late
+
+// versi null safety -> >= 2.12 keatas
+
+// Kesimpulan
+// 1. ? -> diperbolehkan null
+// 2. ?? -> untuk handle null
+// 3. ! -> memastikan bahwa data sudah pasti ada (tidak null)
+// 4. late -> memastikan bahwa sebeluum dieksesusi wajib di inisialisasi
+
 void main() {
-  
+  String? nama = getNama();
+
+  print(nama?.length ?? "TIDAK ADA NAMA");
+  if (nama?.length == null) {
+    // menghandle data null
+    print("TIDAK ADA DATA NAMA");
+  } else {
+    // sudah dipastikan ada datanya
+    print(nama!.length);
+  }
+}
+
+String? getNama() {
+  return "JUMADI";
 }
